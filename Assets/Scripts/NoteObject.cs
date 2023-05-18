17882c432be4156ad69423dc1a390140c5e9ea42
Anh Thu Nguyen <gnguyenanhthu@gmail.com>
2023-05-18 00:28:32 -0400

    Light-UP

---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canbePressed;
+
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canbePressed)
+            {
+                gameObject.SetActive(false);
+
+                //GameManager.instance.NoteHit();
+
+                if(Mathf.Abs(transform.position.y) > 0.25)
+                {
+                    Debug.Log("Hit");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                } else if (Mathf.Abs(transform.position.y) > 0.05f)
+                {
+                    Debug.Log("Good");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, hitEffect.transform.rotation);
+                } else
+                {
+                    Debug.Log("Perfect");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, hitEffect.transform.rotation);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Activator")
+        {
+            canbePressed = true;
+        }    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Activator")
+        {
+            canbePressed = false;
+
+            GameManager.instance.NoteMissed();
+
+            Instantiate(missEffect, transform.position, hitEffect.transform.rotation);
+
+        }
+    }
+}
